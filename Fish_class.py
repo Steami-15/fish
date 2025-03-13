7afc260e17c56cb1eb6afa367498e24e60a7456c
3zDax42 <ezri@thebandit.org>
2025-03-13 15:21:08 -0600

    Update Fish_class.py

--- Fish_class.py
+++ Fish_class.py
@@ -6,40 +6,38 @@ import pygame
 import random
 import time
 
-pygame.init()
-Game_Screen = pygame.display.set_mode((800, 600))
-pygame.display.set_caption("Fish_Tank")
-
-
 class Fish:
     def __init__(self):
         self.Fish_Width = 0
         self.Fish_Height= 0
-        self.Fish_Image = fish_image
+        self.Frame_num = 0
+        self.Hunger = 100
         self.X_Pos = random.randint(0, 800-self.Fish_Width)
         self.Y_Pos = random.randint(0,600-self.Fish_Height)
-        self.is_alive = True
-        self,speed = 1
-        self.direction = 1
-        self.xDir = random.randint(-1,1)
-        self.yDir = random.randint(-1,1)
+        self.Is_Alive = True
+        self.Speed = 1
+        self.Direction = 1
+        self.X_Dir = random.randint(-1,1)
+        self.Y_Dir = random.randint(-1,1)
         self.last_change_time = time.time()
 
     def move(self):
         # Move the fish
-        self.X_Pos += self.xDir* self.speed
-        self.Y_Pos += self.yDir * self.speed
+        self.X_Pos += self.X_Dir* self.Speed
+        self.Y_Pos += self.Y_Dir * self.Speed
 
-        # Change direction every 3 seconds
+        # Change Direction every 3 seconds
         if time.time() - self.last_change_time > 3:  
-            self.xDir = random.randint(-1,1)
-            self.yDir = random.randint(-1,1)
+            self.X_Dir = random.randint(-1,1)
+            self.Y_Dir = random.randint(-1,1)
             self.last_change_time = time.time() #reset the time
 
-        # Check for collision with walls and change direction
+        # Check for collision with walls and change Direction
         if self.X_Pos <= 0 or self.X_Pos >= 800-self.Fish_Width:
-            self.xDir *= -1
+            self.X_Dir *= -1
         if self.Y_Pos <= 0 or self.Y_Pos>= 600-self.Fish_Height:
-            self.yDir *= -1
-    def draw(self, screen):
-        screen.blit(self.fishImage, (self.X_Pos, self.Y_Pos))
+            self.Y_Dir *= -1
+        
+    def draw(self):
+        if self.Is_Alive == True:
+            Game_Screen.blit(self.Fish_Image, (self.X_Pos, self.Y_Pos), (0, self.Fish_Height*self.Frame_num, self.Fish_Width, self.Fish_Height))
